024899fcc9f2be58f04a093f6df9e240a010cfe3
Andrew Niekamp <aniekamp1@udayton.edu>
2022-05-24 15:57:22 -0400

    TSUGI-308 Add images and update description

--- register.php
+++ register.php
@@ -4,6 +4,22 @@ $REGISTER_LTI2 = array(
     "name" => "Photo Gallery", // Name of the tool
     "FontAwesome" => "fa-picture-o", // Icon for the tool
     "short_name" => "Photo Gallery",
-    "description" => "A simple photo gallery tool that allows everyone in a course to upload images to a shared gallery.", // Tool description
-    "messages" => array("launch", "launch_grade")
+    "description" => "Add a photo gallery for images to be uploaded and viewed.", // Tool description
+    "messages" => array("launch", "launch_grade"),
+    "license" => "Apache",
+    "languages" => array(
+        "English"
+    ),
+    "analytics" => array(
+        "internal"
+    ),
+    "video" => "https://udayton.warpwire.com/w/GfsEAA/",
+    "source_url" => "https://github.com/udaytonapps/gallery",
+    "screen_shots" => array(
+        "images/PG-Image-View.jpg",
+        "images/PG-Empty-View.jpg",
+        "images/PG-Image-Upload-and-Editor.jpg",
+        "images/PG-Gallery-View.jpg",
+        "images/PG-Instructor-Gallery-View.jpg"
+    )
 );
